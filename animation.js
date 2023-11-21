cb7ea3a5c452a1723e149c8e86b92fa51f0a49cf
Yogesh Kumar <123798166+YogeshSRIMT@users.noreply.github.com>
2023-11-21 07:33:22 +0530

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,10 @@
+//typing animation
+
+var typed = new Typed(".typing",{
+    strings:[ "","Full stack Developer" , "Web Designer" , "UI/UX Designer" , "Software Engineer",],
+    typedspeed:100,
+    Backspeed:60,
+    loop:true
+})
+
+
